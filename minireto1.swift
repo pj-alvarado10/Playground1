0849a9a486158fe4b74dba4db0a4f1187fd1a7f6
Paula Alvarado <pj.alvarado10@uniandes.edu.co>
2019-07-18 00:05:06 -0500

    Add files via upload

---
+++ minireto1.swift
@@ -0,0 +1,35 @@
+
+var rango = 0...100
+
+
+
+for i in rango{
+
+ 
+if rango%5 == 0{
+
+  print("# \(rango) Bingo!!!")
+
+ }
+
+ if rango%2 == 0{
+
+  print("# \(rango) par!!!")
+ 
+}
+
+ if rango%2 != 0{
+
+  print("# \(rango) impar!!!") 
+
+ }
+ 
+
+if rango>=30 && rango<=40{
+
+  print("# \(rango) Viva Swift!!!")
+ 
+}
+
+
+}
